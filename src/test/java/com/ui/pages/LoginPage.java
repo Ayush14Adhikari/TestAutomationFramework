c72074a9534e4932ebaa9641b0b943db9c3d1bb3
Ayush Adhikari <ayushadhikari93@gmail.com>
2024-12-15 19:53:42 +0530

    updated code

--- src/test/java/com/ui/pages/LoginPage.java
+++ src/test/java/com/ui/pages/LoginPage.java
@@ -9,6 +9,7 @@ public final class LoginPage extends BrowserUtility{
 	private static final By EMAIL_TEXTBOX_LOCATOR = By.id("email");
 	private static final By PASSWORD_TEXTBOX_LOCATOR = By.id("passwd");
 	private static  final By SUBMIT_LOGIN_BUTTON_LOCATOR=By.id("SubmitLogin");
+	private static final By ERROR_MESSAGE_LOCATOR=By.xpath("//div[contains(@class,\"alert-danger\")]/ol/li");    
 	public LoginPage(WebDriver driver) {
 		super(driver);
 		
